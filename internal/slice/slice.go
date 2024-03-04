94d4aeaac38f9d0726ab254df74d019aa3840f71
Старцев Евгений <startsev@corp.sputnik.ru>
2024-03-04 10:49:12 +0300

    Добавить функции удаления элемента из конца слайса, добавление элемента в конец слайса, увелечение каждого элемента слайса на 1

--- internal/slice/slice.go
+++ internal/slice/slice.go
@@ -2,18 +2,20 @@ package slice
 
 // AppendElement добавляет элемент в конец слайса.
 func AppendElement(ints []int, elem int) []int {
-	// Код тут
+	ints = append(ints, elem)
 	return ints
 }
 
 // RemoveElement удаляет последний элемент из слайса. Если массив пуст, функция возвращает оригинальный пустой массив.
 func RemoveElement(ints []int) []int {
-	// Код тут
+	ints = ints[:len(ints)-1]
 	return ints
 }
 
 // AddOneToAll увеличивает каждый элемент массива на единицу.
 func AddOneToAll(ints []int) []int {
-	// Код тут
+	for i := 0; i < len(ints); i++ {
+		ints[i] += 1
+	}
 	return ints
 }
